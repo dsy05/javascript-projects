43c1f8f5d24760a70b57e8b20f48cd795cc1c032
Daniyal Syed <daniyal.syed@live.com>
2023-08-19 13:49:01 -0500

    <unit 1>

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,12 +1,19 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+
+   // not so bad, i guess
+
+   /* hmmmmmm
+   to be or not to be
+   what is this? */
+
 
    /* Here is how
    to have
    multi-line
    comments. */
 
-   console.log("Comments make your code more readable by others.");
+   console.log('"Comments make your code more readable by others."');
--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,7 @@
 console.log('Hello, JavaScript.');
 console.log(2001);
 console.log("What","do","commas","do?");
-console.log("Does", "adding",      "space", "matter?");
+console.log("Does", "adding",      "\nspace", "\tmatter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+console.log('Coding Rocks')
