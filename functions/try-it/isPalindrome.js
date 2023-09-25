71e9213db50a9bfe87c852786afe7d614cf89f13
Daniyal Syed <daniyal.syed@live.com>
2023-09-24 21:48:15 -0500

    <functions exercises>

--- functions/try-it/isPalindrome.js
+++ functions/try-it/isPalindrome.js
@@ -6,11 +6,83 @@
 //    return reverse(str) === str;
 // }
 
-// not good example, want function to do one thing not two
-function isPalindrome(str) {
-   let reversed = str.split('').reverse().join('');
-   return reversed === str;
+// // not good example, want function to do one thing not two
+// function isPalindrome(str) {
+//    let reversed = str.split('').reverse().join('');
+//    return reversed === str;
+// }
+
+// let str = "zerlorez";
+// console.log(isPalindrome(str));
+
+
+const input = require('readline-sync');
+
+function makeLine(size) {
+    let printedLineSize = '';
+
+    if (question.length === 1) {
+        for (let i = 0; i < size; i++) {
+            printedLineSize = question + printedLineSize;
+        } 
+    } else {
+            for (let i = 0; i < size; i++) {
+                printedLineSize = '#' + printedLineSize;
+            }
+        }
+    return printedLineSize;
 }
 
-let str = "zerlorez";
-console.log(isPalindrome(str));
+// function makeSquare(height) {
+//     let printedSquare = '';
+//     for (let i = 1; i <= height; i++) {
+//         printedSquare = '\n' + makeLine(size) + printedSquare;
+//     }
+//     return printedSquare;
+// }
+
+// function makeRectangle(width, height) {
+//     let printedRectangle = '';
+//     for (let i = 0; i < height; i++) {
+//         printedRectangle = printedRectangle + makeLine(width) + '\n';
+//     }
+//     return printedRectangle.slice(0, -1);
+// }
+
+// function makeDownwardStairs(height) {
+//     let printedDownwardStairs = '';
+//     for (let i = 0 ; i < height; i++) {
+//         printedDownwardStairs = printedDownwardStairs + makeLine(i+1) + '\n';
+//     }
+//     return printedDownwardStairs;
+// }
+
+function makeSpaceLine(numSpaces, numChars) {
+    let addedSpaces = makeLine(numChars);
+    for (let i = 0 ; i < numSpaces; i++) {
+         addedSpaces = ' ' + addedSpaces + ' ';
+    }
+    return addedSpaces;
+}
+
+function makeIsoscelesTriangle(height) {
+    let isoscelesTriangle = '';
+    for (let i = 0 ; i < height; i++) {
+        isoscelesTriangle = isoscelesTriangle + '\n' + (makeSpaceLine(height-i+1, 2*i+1));
+     }
+     return isoscelesTriangle;
+}
+
+function makeDiamond(height) {
+    let diamond = makeIsoscelesTriangle(height) + '\n' + makeIsoscelesTriangle(height).split('').reverse().join('');
+    return diamond;
+}
+
+
+//console.log(makeLine(5));
+//console.log(makeSquare(5));
+//console.log(makeRectangle(5, 3));
+//console.log(makeDownwardStairs(5));
+//console.log(makeSpaceLine(3, 5));
+//console.log(makeIsoscelesTriangle(5));
+console.log(makeDiamond(5));
