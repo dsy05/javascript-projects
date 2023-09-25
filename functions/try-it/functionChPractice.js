06ccbd2d605da19ed0cfe0729c5097816768e073
Daniyal Syed <daniyal.syed@live.com>
2023-09-24 21:40:03 -0500

    functions exercises

---
+++ functions/try-it/functionChPractice.js
@@ -0,0 +1,66 @@
+// function countToN(n) {
+//     let count = 1;
+//     while (true) {
+//         if (count > n) {
+//             return;
+//         }
+//         console.log(count);
+//         count++;
+//     }
+// }
+
+// function isEven(n) {
+//     if (n % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(isEven(4));
+// console.log(isEven(7));
+
+// function plusTwo(num) {
+//     return num + 2;
+// }
+
+// let a = 2;
+
+// for (let i=0; i < 4; i++) {
+//     a = plusTwo(a);
+// }
+
+// console.log(a);
+
+// function repeater(str) {
+//     let repeated = str + str;
+//     console.log(repeated);
+// }
+
+// repeater('Bob');
+
+// function makeSandwich(breadType, fillingType, condiments) {
+
+//     // TODO: make a sandwich with the given ingredients
+
+// }
+
+
+// let message = "Hello, World!";
+
+// function printMessage() {
+//     console.log(message);
+// }
+
+// printMessage();
+// message = "Goodbye";
+// printMessage();
+
+
+let num = 42;
+
+function isEven (num) { 
+    return num % 2 === 0; 
+}
+
+console.log(isEven(43));
--- functions/try-it/isPalindrome.js
+++ functions/try-it/isPalindrome.js
@@ -1,7 +1,16 @@
-function reverse(str) {
-   return str.split('').reverse().join('');
+// function reverse(str) {
+//    return str.split('').reverse().join('');
+// }
+
+// function isPalindrome(str) {
+//    return reverse(str) === str;
+// }
+
+// not good example, want function to do one thing not two
+function isPalindrome(str) {
+   let reversed = str.split('').reverse().join('');
+   return reversed === str;
 }
 
-function isPalindrome(str) {
-   return reverse(str) === str;
-}
+let str = "zerlorez";
+console.log(isPalindrome(str));
