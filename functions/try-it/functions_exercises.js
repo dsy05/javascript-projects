88f4100b5e7e835bb0cbb797f44532056aaf3040
Daniyal Syed <daniyal.syed@live.com>
2023-09-26 12:42:27 -0500

    <updated code to exercise>

--- functions/try-it/functions_exercises.js
+++ functions/try-it/functions_exercises.js
@@ -2,31 +2,38 @@ const input = require('readline-sync');
 
 function makeLine(size) {
     let printedLineSize = '';
-
     if (question.length === 1) {
         for (let i = 0; i < size; i++) {
-            printedLineSize = question + printedLineSize;
+            printedLineSize += question;
         } 
     } else {
             for (let i = 0; i < size; i++) {
-                printedLineSize = '#' + printedLineSize;
+                printedLineSize += '#';
             }
         }
     return printedLineSize;
 }
 
-// function makeSquare(height) {
+// function makeSquare(size) {
 //     let printedSquare = '';
-//     for (let i = 1; i <= height; i++) {
-//         printedSquare = '\n' + makeLine(size) + printedSquare;
+//     for (let i = 0; i < size; i++) {
+//         if (i === size - 1) {
+//             printedSquare += makeLine(size) + '\n';
+//         } else {
+//            printedSquare += makeLine(size) + '\n';
+//         }
 //     }
-//     return printedSquare;
+//     return printedSquare.slice(0, -1);
+// }
+
+// function makeSquare(size) {
+//     return makeRectangle (size, size);                       
 // }
 
 // function makeRectangle(width, height) {
 //     let printedRectangle = '';
 //     for (let i = 0; i < height; i++) {
-//         printedRectangle = printedRectangle + makeLine(width) + '\n';
+//         printedRectangle += makeLine(width) + '\n';
 //     }
 //     return printedRectangle.slice(0, -1);
 // }
@@ -34,30 +41,45 @@ function makeLine(size) {
 // function makeDownwardStairs(height) {
 //     let printedDownwardStairs = '';
 //     for (let i = 0 ; i < height; i++) {
-//         printedDownwardStairs = printedDownwardStairs + makeLine(i+1) + '\n';
+//         printedDownwardStairs += makeLine(i+1) + '\n';
 //     }
-//     return printedDownwardStairs;
+//     return printedDownwardStairs.slice(0, -1);
 // }
 
 function makeSpaceLine(numSpaces, numChars) {
-    let addedSpaces = makeLine(numChars);
+    //let addedSpaces = makeLine(numChars);
+    let spaces = '';
     for (let i = 0 ; i < numSpaces; i++) {
-         addedSpaces = ' ' + addedSpaces + ' ';
+         //addedSpaces = ' ' + addedSpaces + ' ';
+         spaces += ' ';
     }
-    return addedSpaces;
+    let chars = makeLine(numChars);
+    return `${spaces}${chars}${spaces}`;
 }
 
 function makeIsoscelesTriangle(height) {
     let isoscelesTriangle = '';
     for (let i = 0 ; i < height; i++) {
-        isoscelesTriangle = isoscelesTriangle + '\n' + (makeSpaceLine(height-i+1, 2*i+1));
+        isoscelesTriangle += `${makeSpaceLine(height-i-1, 2*i+1)}\n`;
      }
-     return isoscelesTriangle;
+     return isoscelesTriangle.slice(0, -1);
 }
 
+function reverse(str) {
+    let reversed = '';
+
+    for (let i = 0; i < str.length; i++) {
+        reversed = str[i] + reversed;
+    }
+
+    return reversed;
+}
 function makeDiamond(height) {
-    let diamond = makeIsoscelesTriangle(height) + '\n' + makeIsoscelesTriangle(height).split('').reverse().join('');
-    return diamond;
+    //let diamond = makeIsoscelesTriangle(height) + '\n' + makeIsoscelesTriangle(height).split('').reverse().join('');
+    let topHalf = makeIsoscelesTriangle(height);
+    let bottomHalf = reverse(topHalf);
+
+    return `${topHalf.slice(0, -(height * 2))}\n${bottomHalf.slice(height * 2)}`;
 }
 
 let question = input.question(`\n What Character would you like to use? `);
@@ -67,5 +89,5 @@ let question = input.question(`\n What Character would you like to use? `);
 //console.log(makeDownwardStairs(5));
 //console.log(makeSpaceLine(3, 5));
 //console.log(makeIsoscelesTriangle(5));
-console.log(makeDiamond(5));
+console.log(makeDiamond(8));
 
