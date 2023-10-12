ac76fbda104145d411513470bf98eedcdd4461be
Daniyal Syed <daniyal.syed@live.com>
2023-10-11 22:45:09 -0500

    <ch14 studio>

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,39 @@
 
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCool: "100%",
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(num) {
+    let outputs = {
+      2: 'Launch',
+      3: 'Code',
+      5: 'Rocks'
+    };
+    let result = "";
+    for (let modulus in outputs) {
+      if (num % Number(modulus) === 0) {
+        if (modulus == 5 && result.length > 0) {
+            result += " ";
+        }
+        result += outputs[modulus];
+      }
+    }
+    if (result.length === 0) {
+      result = "Rutabagas! That doesn't work.";
+    } else {
+      result += "!";
+    }
+    if (result === "Launch Rocks!") {
+      result += " (CRASH!!!!)"
+    }
+    return result;
+  }
+};
+
+
+
 
 module.exports = launchcode;
+    
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,60 @@
-// launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function() {
 
-  // Write your unit tests here!
-  
+  test("should be nonprofit", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("director is jeff", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("percentage of cool employees", function() {
+    expect(launchcode.percentageCool).toBe("100%");
+  });
+
+  test("programs offered", function() {
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+
+  describe("should have a method, launchoutput, which", function() {
+
+    test("should return 'launch!' for numbers divisible only by 2", function() {
+      expect(launchcode.launchOutput(2)).toEqual("Launch!");
+    });
+    
+    test("should return 'Code!' for numbers divisible only by 3", function() {
+      expect(launchcode.launchOutput(3)).toEqual("Code!");
+    });
+
+    test("should return 'Code!' for numbers divisible only by 5", function() {
+      expect(launchcode.launchOutput(5)).toEqual("Rocks!");
+    });
+
+    test("should return 'LaunchCode!' for numbers divisible by 2 and 3", function() {
+      expect(launchcode.launchOutput(6)).toEqual("LaunchCode!");
+    });
+
+    test("should return 'Code Rocks!' for numbers divisible by 3 and 5", function() {
+      expect(launchcode.launchOutput(15)).toEqual("Code Rocks!");
+    });
+
+    //modify 2 and 5 return
+    test("should return 'Launch Rocks!' for numbers divisible by 2 and 5", function() {
+      expect(launchcode.launchOutput(10)).toEqual("Launch Rocks! (CRASH!!!!)");
+    });
+
+    test("should return 'LaunchCode Rocks!' for numbers divisible by 2, 3 and 5", function() {
+      expect(launchcode.launchOutput(30)).toEqual("LaunchCode Rocks!");
+    });
+
+    test("should return 'Rutabagas! That doesn't work.' for numbers not divisible by 2, 3 and 5", function() {
+      expect(launchcode.launchOutput(7)).toEqual("Rutabagas! That doesn't work.");
+    });
+
+  })
 });
