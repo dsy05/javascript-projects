8344abdd1e606a22da1a3b92c60f332b105c488e
Daniyal Syed <daniyal.syed@live.com>
2023-10-31 13:05:44 -0500

    ch 23 exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,36 @@
 function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
+    const abortButton = document.getElementById("abortMission");
+    const liftoffButton = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    liftoffButton.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    liftoffButton.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "green" ;
+    });
+
+    liftoffButton.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor = "";
+     });
     
+    abortButton.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "red" ;
+    });
+
+    abortButton.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor = "";
+     });
+
+     abortButton.addEventListener("click", function() {
+        let shouldAbort = confirm("Are you sure you want to abort the mission?");
+        if (shouldAbort){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home!";
+        }
+     });
+
 }
 
 window.addEventListener("load", init);
